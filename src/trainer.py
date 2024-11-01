e89d0d43c3f80db27bd85eb580cb8724e4dc4c30
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-11-01 08:39:34 +0100

    training class prototype is added

--- src/trainer.py
+++ src/trainer.py
@@ -4,14 +4,22 @@ import argparse
 
 sys.path.append("./src/")
 
-class Trainer():
-    def __init__(self, ):
+
+class Trainer:
+    def __init__(
+        self, features_extraction: bool = False, features_selection: bool = False
+    ):
         self.steps = "training"
-        
+        self.features_extraction = features_extraction
+        self.features_selection = features_selection
+
+    def feature_extraction_technique(self):
+        pass
+
     def train(self):
         pass
-    
-    
+
+
 if __name__ == "__main__":
     trainer = Trainer()
     trainer.train()
