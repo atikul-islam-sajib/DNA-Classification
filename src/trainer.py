5fdefb352f1ea730839997af2f40246d2d6acd4c
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-11-01 11:13:31 +0100

    trainer class is added and needs to be added the code later

--- src/trainer.py
+++ src/trainer.py
@@ -11,51 +11,25 @@ from sklearn.preprocessing import StandardScaler
 sys.path.append("./src/")
 
 from utils import config
+from helper import features_extraction_techquiue
 
 
 class Trainer:
     def __init__(
-        self, features_extraction: bool = False, features_selection: bool = False
+        self,
+        model: str = "RF",
+        features_extraction: bool = False,
+        features_selection: bool = False,
     ):
         self.steps = "training"
         self.features_extraction = features_extraction
         self.features_selection = features_selection
 
-        self.number_of_components = 20
-
-        self.X_train = os.path.join(config()["path"]["processed_path"], "X_train.csv")
-        self.X_test = os.path.join(config()["path"]["processed_path"], "X_test.csv")
-        self.y_train = os.path.join(config()["path"]["processed_path"], "y_train.csv")
-        self.y_test = os.path.join(config()["path"]["processed_path"], "y_test.csv")
-
-        self.X_train = pd.read_csv(self.X_train)
-        self.X_test = pd.read_csv(self.X_test)
-        self.y_train = pd.read_csv(self.y_train)
-        self.y_test = pd.read_csv(self.y_test)
-
-        self.training_dataset = pd.concat([self.X_train, self.y_train], axis=1)
-        self.testing_dataset = pd.concat([self.X_test, self.y_test], axis=1)
-
-        self.training_dataset = self.training_dataset.iloc[:, 4:]
-        self.testing_dataset = self.testing_dataset.iloc[:, 4:]
-
-        print(self.training_dataset.head(2))
-        print(self.training_dataset.isnull().sum().sum())
-
-        print(self.training_dataset.iloc[2, :].values)
-
-    def feature_extraction_technique(self):
-        self.explained_variances = []
-
-        print(self.training_dataset.shape, self.training_dataset.isnull().sum().sum())
-        print(self.training_dataset.head())
-
-        decompositoon = PCA()
-        decompositoon.fit_transform(self.training_dataset)
-        print(decompositoon.explained_variance_ratio_)
-
     def train(self):
-        pass
+        if self.features_extraction:
+            dataset = features_extraction_techquiue()
+        elif self.features_selection:
+            pass
 
 
 if __name__ == "__main__":
